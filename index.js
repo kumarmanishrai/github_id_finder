6c7da0c38208c57d6c6a547753457b39e5b738a9
ManishKumarRai-creator <98271764+kumarmanishrai@users.noreply.github.com>
2023-08-07 17:26:49 +0530

    add

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ let modeText = document.querySelector('#mode-text');
 let modeIcon = document.querySelector('.icon-container');
 let profileContent = document.querySelector('.profile-content');
 let userName = document.querySelector('#name');
+let avatar = document.querySelector('#avatar');
 let userId = document.querySelector('#user');
 let repos = document.querySelector('#repos');
 let followers = document.querySelector('#followers');
 let following = document.querySelector('#following');
+let buttonContainer = document.querySelector('.button-container');
+
 modeText.addEventListener('click',() => {
     profileContent.classList.add("dark-mode");
 });
@@ -18,11 +21,14 @@ modeIcon.addEventListener('click',() => {
 let input = document.querySelector('#input');
 let submit = document.querySelector('#submit');
 submit.addEventListener('click',() => {
+    searchFollowers(input.value);
     searchUserName(input.value);
 });
 input.addEventListener('keydown',(event) => {
     if(event.keyCode === 13){
-        searchUserName(input.value);
+      searchFollowers(input.value);
+      searchUserName(input.value);
+
     }
 })
 
@@ -36,6 +42,7 @@ async function searchUserName(value){
         // Check if the response status code indicates success (200) or not found (404)
         if (response.status === 200) {
           const data = await response.json();
+          console.log(data);
           renderData(data);
         } else if (response.status === 404) {
           console.log('User not found');
@@ -62,7 +69,8 @@ async function searchUserName(value){
 
 function renderData(data){
     userName.innerHTML = data?.name;
-    userId.innerHTML = `@${userName.innerHTML}`;
+    avatar.src = data?.avatar_url;
+    userId.innerHTML = data.login;
     userId.href = data?.html_url;
     repos.innerHTML = data?.public_repos;
     followers.innerHTML = data?.followers;
@@ -78,9 +86,11 @@ const clearButton = document.createElement('button');
 clearButton.setAttribute('class', 'clear-button');
 clearButton.setAttribute('type', 'button');
 clearButton.textContent = 'X';
+clearButton.style.display = 'none';
 
 // Append the clear button to the input container
 searchBarConatiner.appendChild(clearButton);
+buttonContainer.appendChild(clearButton);
 
 // Show the clear button when the input field is not empty
 input.addEventListener('input', function() {
@@ -103,3 +113,56 @@ clearButton.addEventListener('click', function() {
 
 
 
+ // Replace '{value}' with the desired GitHub username
+
+const container = document.getElementById('userContainer');
+
+function createCard(userData) {
+  const card = document.createElement('div');
+  card.classList.add('user-card');
+
+  const avatar = document.createElement('img');
+  avatar.classList.add('user-avatar');
+  avatar.src = userData.avatar_url;
+  avatar.alt = userData.login;
+
+  const login = document.createElement('div');
+  login.classList.add('user-login');
+  login.textContent = userData.login;
+  console.log(userData.login);
+
+  const link = document.createElement('a');
+  link.classList.add('user-link');
+  link.href = userData.html_url;
+  link.target = '_blank';
+  link.textContent = 'Visit Profile';
+
+  card.appendChild(avatar);
+  card.appendChild(login);
+  card.appendChild(link);
+
+  container.appendChild(card);
+}
+
+// Fetch followers data from GitHub API
+
+async function searchFollowers (value){
+
+  await fetch(`https://api.github.com/users/${value}/followers`)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Loop through the followersData array and create a card for each follower
+    console.log(data);
+    container.innerHTML = '';
+    data.forEach(follower => createCard(follower));
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+}
+  
